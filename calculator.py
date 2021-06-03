7cdcb050e705e89afb2359449235992dcb8462e7
ISHAKU <lubishak@gmail.com>
2021-06-03 10:29:39 +0100

    first commit

---
+++ calculator.py
@@ -0,0 +1,21 @@
+class Calculator:
+
+    def add(a, b):
+        return a + b
+
+    
+    def divide(mango, student):
+        return mango / student
+
+
+    def mul(x, y):
+        return x * y
+
+
+    def sub(orange, monkey):
+        return orange - monkey
+
+
+calculator = Calculator
+mul = calculator.sub(15, 10)
+print(mul)
